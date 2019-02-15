c21843c3fbdbc638d7e4488fea0955d57776bcbb
Ogurinka Benjamin <ogurinkabenjamin@gmail.com>
2019-02-15 14:21:07 +0100

    Enable navbar toggle

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,19 @@
-//sidebar toggle
 $(document).ready(function() {
+          //sidebar toggle
           $('.sidebar-toggle').on('click', function() {
                     $('#tp-nav').toggleClass('is-active');
 
+          });
+
+          //close sidebar
+          var $sidebar = $('#tp-nav');
+
+          $(document).mouseup(function(e) {
+
+                    if (!$sidebar.is(e.target) &&
+                              $sidebar.has(e.target).length === 0) {
+                              $sidebar.removeClass('is-active');
+                    }
           });
 
 });
