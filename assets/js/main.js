3ac6b5b63482ca318a841892e27d481375842ddb
Ogurinka Benjamin <ogurinkabenjamin@gmail.com>
2019-02-21 13:41:14 +0100

    Add active class to submenu toggle

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,19 @@
 $(document).ready(function() {
-          //sidebar toggle
-          $('.sidebar-toggle').on('click', function() {
-                    $('#tp-nav').toggleClass('is-active');
-
-          });
-          //submenu 
-          $('.sub-toggle').on('click', function() {
-                    $('#sub-menu').toggleClass('is-active');
-
-          });
-
-          //close sidebar
-          var $sidebar = $('#tp-nav');
-
-          $(document).mouseup(function(e) {
-
-                    if (!$sidebar.is(e.target) &&
-                              $sidebar.has(e.target).length === 0) {
-                              $sidebar.removeClass('is-active');
-                    }
-
-          });
-
+	//sidebar toggle
+	$('.sidebar-toggle').on('click', function() {
+		$('#tp-nav').toggleClass('is-active');
+	});
+	//submenu 
+	$('.sub-toggle').on('click', function() {
+		$('#sub-menu').toggleClass('is-active');
+		$('.sub-toggle').toggleClass('text-active');
+	});
+	//close sidebar
+	var $sidebar = $('#tp-nav');
+     $(document).on('mouseup', function(e) {
+		if (!$sidebar.is(e.target) &&
+			$sidebar.has(e.target).length === 0) {
+			$sidebar.removeClass('is-active');
+		}
+	});
 });
