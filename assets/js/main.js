95fb69b5cb324b57d08dc5cd541bf5067a5e87bf
Ogurinka Benjamin <ogurinkabenjamin@gmail.com>
2019-03-15 06:53:43 +0100

    Update via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,12 @@ $(document).ready(function() {
 	$('.sidebar-toggle').on('click', function() {
 		$('#tp-nav').toggleClass('became-active');
 	});
-	//close sidebar
+	//submenu
+	// $('.sub-toggle').on('click', function() {
+	// 	$('#sub-menu').toggleClass('became-active');
+	// 	$('.sub-toggle').toggleClass('text-active');
+	// });
+	// close sidebar
 	var $sidebar = $('#tp-nav');
 	$(document).on('mouseup', function(e) {
 		if (!$sidebar.is(e.target) && $sidebar.has(e.target).length === 0) {
